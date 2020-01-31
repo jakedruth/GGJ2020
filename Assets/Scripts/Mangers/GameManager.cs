327a5f56e514c5da6020592093b856834a1845ec
Jakedruth@gmail.com <Jakedruth@gmail.com>
2020-01-31 15:54:23 -0500

    Initial Unity Push

---
+++ Assets/Scripts/Mangers/GameManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+    public GameData data;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        if(instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+}
+
+public struct GameData
+{
+}
