853b652734aaf9538bbe6c7648917c6254212571
Tremaine Phinney <tremainephinney@gmail.com>
2020-01-31 23:40:16 -0500

    More UI Stuff
    -Main Menu done
    -Starting Pause Menu

---
+++ Assets/Scripts/Handlers/MainMenuHandler.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuHandler : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+    public void playGame()
+    {
+        //Load next Scene...
+    }
+    public void closeGame()
+    {
+        Application.Quit();
+    }
+}
