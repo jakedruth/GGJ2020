356227db6822c43472fb97ef9102633ec97f3df2
Jakedruth@gmail.com <Jakedruth@gmail.com>
2020-02-02 12:51:30 -0500

    Fixed The Rope

--- Assets/Scripts/Entities/RopeController.cs
+++ Assets/Scripts/Entities/RopeController.cs
@@ -14,13 +14,13 @@ public class RopeController : MonoBehaviour
     void Awake()
     {
         _line = GetComponent<LineRenderer>();
-        _line.SetPosition(0, Vector3.zero);
-        _line.SetPosition(1, Vector3.zero);
+        _line.SetPosition(0, Vector3.forward * -0.25f);
+        _line.SetPosition(1, Vector3.forward * -0.25f);
     }
 
     public void SetRopeEndPoint(Vector3 point)
     {
-        _line.SetPosition(1, point);
+        _line.SetPosition(1, point + Vector3.forward * -0.25f);
     }
 
     public void SendOutRopeToPoint(Vector3 point)
@@ -55,7 +55,7 @@ public class RopeController : MonoBehaviour
         while (pos != target)
         {
             pos = Vector3.MoveTowards(pos, target, ropeMoveSpeed * Time.deltaTime);
-            _line.SetPosition(1, pos);
+            _line.SetPosition(1, pos + Vector3.forward * -0.25f);
 
             yield return null;
         }
@@ -72,14 +72,14 @@ public class RopeController : MonoBehaviour
         while (pos != target)
         {
             pos = Vector3.MoveTowards(pos, target, ropeMoveSpeed * Time.deltaTime);
-            _line.SetPosition(1, pos);
+            _line.SetPosition(1, pos + Vector3.forward * -0.25f);
             yield return null;
         }
 
         while (pos != start)
         {
             pos = Vector3.MoveTowards(pos, start, ropeMoveSpeed * Time.deltaTime);
-            _line.SetPosition(1, pos);
+            _line.SetPosition(1, pos + Vector3.forward * -0.25f);
             yield return null;
         }
 
@@ -95,7 +95,7 @@ public class RopeController : MonoBehaviour
         while (pos != target)
         {
             pos = Vector3.MoveTowards(pos, target, ropeMoveSpeed * Time.deltaTime);
-            _line.SetPosition(1, pos);
+            _line.SetPosition(1, pos + Vector3.forward * -0.25f);
             yield return null;
         }
 
@@ -129,7 +129,7 @@ public class RopeController : MonoBehaviour
         {
             Vector3 offset = _followTarget.position - transform.position;
             pos = Vector3.MoveTowards(pos, offset, ropeMoveSpeed * Time.deltaTime);
-            _line.SetPosition(1, pos);
+            _line.SetPosition(1, pos + Vector3.forward * -0.25f);
             yield return null;
         }
 
