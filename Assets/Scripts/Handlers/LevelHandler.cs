eb330701243879546e66cfa014a760dee950e1c2
Tremaine Phinney <tremainephinney@gmail.com>
2020-02-01 23:16:22 -0500

    Added tooltip to level 3

--- Assets/Scripts/Handlers/LevelHandler.cs
+++ Assets/Scripts/Handlers/LevelHandler.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class LevelHandler : MonoBehaviour
 {
+    public List<AnimalController> animalList;
     // Start is called before the first frame update
     void Start()
     {
-        
+        foreach (AnimalController animal in FindObjectsOfType<AnimalController>())
+        {
+            animalList.Add(animal);
+        }
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        if (animalList.Count != 0)
+        {
+            for (int i = animalList.Count - 1; i >= 0; i--)
+            {
+                if (animalList[i] == null)
+                {
+                    animalList.Remove(animalList[i]);
+                }
+            }
+        }
+        else
+        {
+            //end level
+        }
     }
 }
